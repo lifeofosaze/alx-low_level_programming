ec71ff2d05440ebee604c043397272e584cd4a1e
Rasheed Musa Osaze Musa <musa.rasheed100@gmail.com>
2022-04-20 06:48:20 -0700

    0 Task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+ #include "main.h" 
  /** 
   * reset_to_98 - reset argument to 98 
   * @n: pointer 
@@ -8,6 +8,6 @@
  void reset_to_98(int *n) 
  { 
   
-         *n = 98; 
+ 	*n = 98; 
   
  }
