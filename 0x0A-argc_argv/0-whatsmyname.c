15339145b07dbbe795a3273e50428f6daf45f865
Rasheed Musa Osaze Musa <musa.rasheed100@gmail.com>
2022-05-03 15:14:15 -0700

    0 Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
 
 /**
  * main - Print the name of the program
- * @argc: Count ar (Success)
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
-{guments
- * @argv: Arguments
- *
- * Return: Always 0
+{
 (void) argc;
 printf("%s\n", argv[0]);
 
