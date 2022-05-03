58147f0687ce90343ff256aeaaf3abc7cf834659
Rasheed Musa Osaze Musa <musa.rasheed100@gmail.com>
2022-05-03 08:01:23 -0700

    putchar

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to prineturned, and errno is set appropriately.
+ */
+int _t
+ *
+ * Return: On success 1.
+ * On error, -1 is rputchar(char c)
+{
+return (write(1, &c, 1));
+}
