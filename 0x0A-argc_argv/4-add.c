e75efdd54063c2ca10af7dcb7d8f5bcec397d20e
Rasheed Musa Osaze Musa <musa.rasheed100@gmail.com>
2022-05-04 06:23:36 -0700

    4 Task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,73 +1,89 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+	#include <stdlib.h>
+	#include <ctype.h>
+	#include <string.h>
+	
 
-/**
- * check_num - check - string there are digit
- * @str: array str
- * count;
+	/**
+	 * check_num - check - string there are digit
+	 * @str: array str
+	 *
+	 * Return; Always 0 (Success)
+	 */
+	
 
-count = 0;
-while (count < strlen(str)) /*
- * Return; Always 0 (Success)
- */
+	int check_num(char *str)
+	
 
-int check_num(char *str)
+	{
+	/*Declaring variables*/
+	unsigned int count;
+	
 
-{
-/*Declaring variables*/
-unsigned intcount string*/
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+	
 
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
-}
+	{
+	if (!isdigit(str[count])) /*check if str there are digit*/
+	{
+	return (0);
+	}
+	
 
-count++;
-}
-return (1);
-}
+	count++;
+	}
+	return (1);
+	}
+	
 
-/**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * return: Always 0 (Success)
- */
+	/**
+	 * main - Print the name of the program
+	 * @argc: Count arguments
+	 * @argv: Arguments
+	 *
+	 * return: Always 0 (Success)
+	 */
+	
 
-inring to int*/
-sum += str_to_int;
-}
+	int main(int argc, char *argv[])
+	
 
-/*Condition if one of the number contains symbols that are not dt main(int argc, char *argv[])
+	{
+	
 
-{
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+	
 
-/*Declaring variables*/
-int count;
-int str_to_int;
-int sum = 0;
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
+	{
+	if(check_num(argv[count]))
+	
 
-count = 1;
-while (count < argc) /*Goes through the whole array*/
-{
-if(check_num(argv[count]))
+	{
+	str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+	sum += str_to_int;
+	}
+	
 
-{
-str_to_int = atoi(argv[count]); /*ATOI --> convert stigits*/
-else
-{
-printf("Error\n");
-return (1);
-}
+	/*Condition if one of the number contains symbols that are not digits*/
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+	
 
-count++;
-}
+	count++;
+	}
+	
 
-printf("%d\n", sum); /*print sum*/
+	printf("%d\n", sum); /*print sum*/
+	
 
-return (0);
-}
+	return (0);
+	}
