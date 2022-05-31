fe83beba51a91e2f0c0de021f8d37e7ce52facdd
Rasheed Musa Osaze Musa <musa.rasheed100@gmail.com>
2022-05-30 18:12:00 -0700

    0 task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,33 @@
-#include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
+ * binary_to_uint - converts a binary number to an unsigned int
  *
- * Return: the converted number
+ * @b: binary character
+ *
+ * Return: decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-    unsigned int dec_val = 0;
+	int result = 0, cLoop = 0;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
+	while (b[cLoop])
+	{
+		switch (b[cLoop])
+		{
+			case '0':
+				result *= 2;
+			break;
+			case '1':
+				result *= 2;
+				result += 1;
+			break;
+			default:
+				return (0);
+			break;
+		}
+		cLoop++;
+	}
 
-    for (i = 0; b[i]; i++)
-    {
-        if (b[i] < '0' || b[i] > '1')
-            return (0);
-        dec_val = 2 * dec_val + (b[i] - '0');
-    }
-
-    return (dec_val);
+	return (result);
 }
